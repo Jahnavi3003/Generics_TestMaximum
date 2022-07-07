c63e205e6da866d3f01dc02a0abda29ca8c01a8e
Jahnavi3003 <sujayithimulkj30.com>
2022-07-07 16:25:49 +0530

    Refactoring into Generic Method

--- src/com/generics/ComputeMax.java
+++ src/com/generics/ComputeMax.java
@@ -2,59 +2,36 @@ package com.generics;
 
 public class ComputeMax {
 
-public  void intMax(Integer n1, Integer n2, Integer n3) {
+public <T extends Comparable <T>> T max(T n1, T n2, T n3) {
+	     
+	
+	    T max;
 		
 		if((n1.compareTo(n2)>0) && (n1.compareTo(n3)>0))
 		{
-			System.out.println(n1 + " is maximum");
+			max=n1;
+			System.out.println(n1+ " is maximum");
 		}
 		else if((n2.compareTo(n1)>0) && (n2.compareTo(n3)>0))
 		{
-			System.out.println(n2 + " is maximum");
+			max=n2;
+			System.out.println(n2+ " is maximum");
 		}
 		else
 		{
+			max=n3;
 			System.out.println(n3 + " is maximum");
 		}	
+		return max;
 	}
 	
-public  void floatMax(Float n1, Float n2, Float n3) {
-	
-	if((n1.compareTo(n2)>0) && (n1.compareTo(n3)>0))
-	{
-		System.out.println(n1 + " is maximum");
-	}
-	else if((n2.compareTo(n1)>0) && (n2.compareTo(n3)>0))
-	{
-		System.out.println(n2 + " is maximum");
-	}
-	else
-	{
-		System.out.println(n3 + " is maximum");
-	}	
-}
 
-public  void stringMax(String n1, String n2, String n3) {
-	
-	if((n1.compareTo(n2)>0) && (n1.compareTo(n3)>0))
-	{
-		System.out.println(n1 + " is maximum");
-	}
-	else if((n2.compareTo(n1)>0) && (n2.compareTo(n3)>0))
-	{
-		System.out.println(n2 + " is maximum");
-	}
-	else
-	{
-		System.out.println(n3 + " is maximum");
-	}	
-}
 
 	public static void main(String[] args) {
-		ComputeMax max = new ComputeMax();
-		max.intMax(3,8,1);
-		max.floatMax(3.5F,6.4F,4.2F);
-		max.stringMax("Me", "Mine", "Myself");
+		ComputeMax m = new ComputeMax();
+		m.max(3,8,1);
+		m.max(3.5F,6.4F,4.2F);
+		m.max("Me", "Mine", "Myself");
 		
 	}
 }
